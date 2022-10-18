c0127f92ca72247da7eb99ced49296ff52124287
smalldirtybird <smalldirtybird@gmail.com>
2022-10-18 03:01:13 +0300

    Recalculate min_date taking into account assembly time

--- spreadsheets_api.py
+++ spreadsheets_api.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from pprint import pprint
 
 import gspread
@@ -13,14 +13,22 @@ def get_delivery_date_requirements(google_credentials, table_name, sheet_name,
     delivery_date_requirements = {}
     spreadsheet = worksheet.get_all_values()[1:]
     for row in spreadsheet:
-        min_date = row[1]
-        current_date = row[4]
+        if row[4]:
+            current_delivery_date = datetime.strptime(
+                row[4],
+                '%d.%m.%Y',
+            ).date()
+        min_date = datetime.strptime(row[1], '%d.%m.%Y').date()
+        assembly_time = timedelta(days=int(row[3]))
+        today = datetime.now().date()
+        if min_date - today < assembly_time:
+            min_date = today + assembly_time
         if row[5] == account_name:
             delivery_date_requirements[row[0]] = {
-                'min_date': datetime.strptime(min_date, '%d.%m.%Y').date(),
+                'min_date': min_date,
                 'max_date': datetime.strptime(row[2], '%d.%m.%Y').date(),
-                'assembly_time': row[3],
-                'current_delivery_date': current_date,
+                'assembly_time': assembly_time,
+                'current_delivery_date': current_delivery_date,
                 'current_delivery_date_cell_coordinates':
                     f'E{spreadsheet.index(row) + 2}',
             }
