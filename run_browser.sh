6b4b8a9e1d7efb72f8cf3ffdac288e6bf76e9284
smalldirtybird <smalldirtybird@gmail.com>
2022-11-30 21:04:21 +0300

    Add wait fo load elements

--- run_browser.sh
+++ run_browser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-            firefox -profile "/home/drew/.mozilla/firefox/0q26uo9f.default/" --new-tab "https://seller.ozon.ru/app/supply/orders?filter=SupplyPreparation" --headless &
+            firefox -profile "/home/drew/.mozilla/firefox/9oxl2ewh.default/" --new-tab "https://seller.ozon.ru/app/supply/orders?filter=SupplyPreparation" --headless &
             sleep 10
             kill -9 $!
             kill -9 $!
